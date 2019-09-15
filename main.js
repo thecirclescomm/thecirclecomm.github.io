eed73411dd6a6fe106396c10eb8b59b98b1e390a
Taranjeet Singh <reachtotj@gmail.com>
2019-09-15 16:14:59 +0530

    final commit.

---
+++ main.js
@@ -0,0 +1,44 @@
+(function() {
+  function circle() {}
+
+  circle.prototype.init = function() {
+    $('.multiple-items').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+  }
+
+  var circleObj = new circle();
+  circleObj.init();
+})()
